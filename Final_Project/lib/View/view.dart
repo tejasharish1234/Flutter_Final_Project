c3c9382f0ac735e06950e8417538d68a2bcbc4ee
RajeevMangalapalli <145272734+RajeevMangalapalli@users.noreply.github.com>
2023-10-06 22:49:38 +0530

    Mapslink_left

--- Final_Project/lib/View/view.dart
+++ Final_Project/lib/View/view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../Controller/control.dart';
 
+
+
 class CountryScreen extends StatefulWidget {
   final CountryController controller;
   const CountryScreen({Key? key, required this.controller}) : super(key: key);
@@ -73,7 +75,7 @@ class _CountryScreenState extends State<CountryScreen> {
                 foregroundColor: Colors.blueAccent,
                 backgroundColor: Colors.white,
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
               ),
               child: Text('Search',
                   style: GoogleFonts.lora(
@@ -137,8 +139,8 @@ class _CountryDetailsScreenState extends State<CountryDetailsScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(15.0),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center, // Center alignment
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Container(
                           alignment: Alignment.topCenter,
@@ -146,26 +148,40 @@ class _CountryDetailsScreenState extends State<CountryDetailsScreen> {
                             height: 200,
                             width: 300,
                             child: Image.network(
-                              // Display flag image using Image.network
                               countryModel.flagpng,
                               fit: BoxFit.contain,
                             ),
                           ),
                         ),
                         const SizedBox(height: 100),
-                        InfoTile(
-                            title: 'Population',
-                            value: countryModel.population),
-                        InfoTile(
-                            title: 'Capital', value: countryModel.capitalcity),
-                        InfoTile(title: 'Region', value: countryModel.region),
-                        InfoTile(
-                            title: 'Currency', value: countryModel.currency),
-                        InfoTile(
-                            title: 'Language', value: countryModel.language),
-                        InfoTile(
-                            title: 'Time Zone', value: countryModel.timezone),
-                        const InfoTile(title: 'Calling Code', value: '+123'),
+                        _InfoTile(
+                          title: 'Population',
+                          value: countryModel.population,
+                        ),
+                        _InfoTile(
+                          title: 'Capital',
+                          value: countryModel.capitalcity,
+                        ),
+                        _InfoTile(
+                          title: 'Region',
+                          value: countryModel.subregion,
+                        ),
+                        _InfoTile(
+                          title: 'Currency',
+                          value: countryModel.currency,
+                        ),
+                        _InfoTile(
+                          title: 'Language',
+                          value: countryModel.language,
+                        ),
+                        _InfoTile(
+                          title: 'Time Zone',
+                          value: countryModel.timezone,
+                        ),
+                        _InfoTile(
+                          title: 'Maps Link',
+                          value: countryModel.mapslink,
+                        ),
                         const SizedBox(height: 20),
                         Align(
                           alignment: Alignment.bottomRight,
@@ -189,30 +205,49 @@ class _CountryDetailsScreenState extends State<CountryDetailsScreen> {
   }
 }
 
-class InfoTile extends StatelessWidget {
+class _InfoTile extends StatelessWidget {
   final String title;
   final String value;
 
-  const InfoTile({super.key, required this.title, required this.value});
+  const _InfoTile({
+    required this.title,
+    required this.value,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        title,
-        textAlign: TextAlign.center,
-        style: GoogleFonts.robotoSlab(
-          color: Colors.yellow,
-          fontSize: 20,
-          fontWeight: FontWeight.w900,
-        ),
+    return Container(
+      width:400,
+      decoration: BoxDecoration(
+        color: Colors.teal, // Teal background color
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
       ),
-      subtitle: Text(
-        value,
-        textAlign: TextAlign.center,
-        style: GoogleFonts.robotoSlab(
-          color: Colors.yellow,
-          fontSize: 16,
+      margin: const EdgeInsets.symmetric(vertical: 8.0), // Vertical spacing
+      child: ListTile(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.robotoSlab(
+            color: Colors.yellow,
+            fontSize: 20,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        subtitle: Text(
+          value,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.robotoSlab(
+            color: Colors.yellow,
+            fontSize: 16,
+          ),
         ),
       ),
     );
